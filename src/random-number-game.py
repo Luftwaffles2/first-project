a4f692b2caa181f22a5cfa3ae4d90c0c5d66362e
Luftwaffles2 <august.burke@outlook.com>
2023-12-12 13:41:18 -0600

    check if answer is correct

--- src/random-number-game.py
+++ src/random-number-game.py
@@ -4,10 +4,11 @@ guess_range = 99
 answer= random.randint(1, guess_range)
 
 print("This is a game where you will guess a random number!")
-print ""
+print; ""
 userInput = input("Guess a number between 1 and " + str(guess_range) + ": ")
 guess=int(userInput)
 
 guess= ""
-while != answer
-    
+while  guess != answer:""
+userInput=input (" + str(guess_range) + ")
+guess=int(userInput)
