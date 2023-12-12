7ab76494d201e4bdd466816b04bb0d8404513ddd
Luftwaffles2 <august.burke@outlook.com>
2023-12-12 12:59:47 -0600

    rough draft

--- src/random-number-game.py
+++ src/random-number-game.py
@@ -4,5 +4,10 @@ guess_range = 99
 answer= random.randint(1, guess_range)
 
 print("This is a game where you will guess a random number!")
+print ""
 userInput = input("Guess a number between 1 and " + str(guess_range) + ": ")
 guess=int(userInput)
+
+guess= ""
+while != answer
+    
